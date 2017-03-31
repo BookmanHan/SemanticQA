1ee03ad3a061f38a8f1fe1ec8323896c48925d56
Xiaohan Bookman <bookman@vip.163.com>
2017-03-31 16:09:18 +0800

    Configurations.

--- DetailedConfig.hpp
+++ DetailedConfig.hpp
@@ -2,6 +2,7 @@
 #include "Import.hpp"
 #include "DataSet.hpp"
 
+#ifdef WINConfig
 const string report_path = "H:\\Report\\Semantics\\";
 const string model_path = "H:\\Model\\Semantics\\";
 const string image_reader = "\"C:\\Program Files (x86)\\2345Soft\\2345Pic\\2345PicEditor.exe\" ";
@@ -14,3 +15,19 @@ const DataSet FB15K(
 	"H:\\Data\\Knowledge Embedding\\FB15K\\test.bin",
 	"H:\\Data\\Knowledge Embedding\\FB15K\\dev.bin",
 	"H:\\Data\\Knowledge Embedding\\FB15K\\words.bin");
+#endif
+
+#ifdef LinuxConfig
+const string report_path = "/home/bookman/report";
+const string model_path = "/home/bookman/report";
+const string image_reader = "\"C:\\Program Files (x86)\\2345Soft\\2345Pic\\2345PicEditor.exe\" ";
+const int parallel_number = 16;
+
+const DataSet FB15K(
+	"FB15K",
+	"/home/bookman/FB15K/train.bin",
+	"/home/bookman/FB15K/description.bin",
+	"/home/bookman/FB15K/test.bin",
+	"/home/bookman/FB15K/dev.bin",
+	"/home/bookman/FB15K/words.bin");
+#endif
--- Storage.hpp
+++ Storage.hpp
@@ -10,7 +10,7 @@ public:
 	static Storage* global_system;
 
 public:
-	void open_file(const string& file_name, const string& file_path, const ios_base::open_mode open_mode = ios::binary)
+	void open_file(const string& file_name, const string& file_path, const ios_base::openmode open_mode = ios::binary)
 	{
 		storage_system[file_name] = new fstream(file_path.c_str(), open_mode);
 		if (storage_system[file_name]->bad() || storage_system[file_name]->fail())
